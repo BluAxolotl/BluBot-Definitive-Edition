daebdff78e46355bd4b213fb14219e7faa61a3c4
Blu <plush.gamer.456@gmail.com>
2024-04-14 11:59:54 -0400

    Fuck you per channel VC role now

--- voice_chat_role.js
+++ voice_chat_role.js
@@ -5,21 +5,25 @@ const print = console.log
 module.exports = (async function (s_old, s_new, client, LowSync, JSONFileSync) {
 	const db = new LowSync(new JSONFileSync(`${__dirname}/storage.json`))
 	db.read()
+
+	var s_current_id = (s_old.channelId || s_new.channelId)
+
 	var guild = await client.guilds.fetch(s_new.guild)
 	var gid = guild.id
+	var rid = `${gid}_${s_current_id}`
 	var client_member = await guild.members.fetch(client.user.id)
 	// print("\n\nCan manage roles?")
 	if (client_member.permissions.has("MANAGE_ROLES")) {
 		// print("Yes; Guild in low database?")
-		if (db.data.vc_roles[gid] == null) {
+		if (db.data.vc_roles[rid] == null) {
 			// print("No; Creating role....")
 			let role = await guild.roles.create({
-				name: "Voice Chat",
+				name: `[VC] ${s_new.channel.name}`,
 				color: "BLUE",
 				reason: "Role to ping people in voice chat",
 				mentionable: true
 			})
-			db.data.vc_roles[gid] = role.id
+			db.data.vc_roles[rid] = role.id
 			db.write()
 			db.read()
 		}
@@ -28,7 +32,7 @@ module.exports = (async function (s_old, s_new, client, LowSync, JSONFileSync) {
 			let member = await guild.members.fetch(s_new.id)
 			// print("Got Member...\nRemoving Voice Chat role...")
 			try {
-				await member.roles.remove(db.data.vc_roles[gid])
+				await member.roles.remove(db.data.vc_roles[rid])
 			} catch (err) {
 				print(err)
 			}
@@ -37,7 +41,7 @@ module.exports = (async function (s_old, s_new, client, LowSync, JSONFileSync) {
 			let member = await guild.members.fetch(s_new.id)
 			// print("Got Member...\nAdding Voice Chat role...")
 			try {
-				await member.roles.add(db.data.vc_roles[gid])
+				await member.roles.add(db.data.vc_roles[rid])
 			} catch (err) {
 				print(err)
 			}
